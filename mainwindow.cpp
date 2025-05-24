dd79087e8ea6397f235d16beb018cf98e5625e7d
Pawedloo <pavelgah0@gmail.com>
2025-05-24 19:38:44 +0300

    Add audiofile

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QFileDialog>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -11,4 +13,20 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_openButton_clicked()
+{
+    // Открываем диалог выбора аудиофайла
+    QString filePath = QFileDialog::getOpenFileName(
+        this,
+        tr("Open Audio File"),
+        QDir::homePath(),
+        tr("Audio Files (*.mp3 *.wav *.ogg *.flac)")
+        );
+
+    if (!filePath.isEmpty()) {
+        // Выводим путь к файлу в консоль (можно заменить на нужную вам логику)
+        qDebug() << "Selected audio file:" << filePath;
+    }
 }
